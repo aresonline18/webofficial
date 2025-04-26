70ed30df131bb97045d999dc46271b863dbf15ae
Vlad Prangati <vladprangati@gmail.com>
2025-04-26 15:40:38 +0300

    toate schimbarile facute

--- script/header.js
+++ script/header.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', () => {
     const header = document.getElementById('main-header');
     if (window.scrollY > 30) {
-      header.style.backgroundColor = '#111'; // darker shade
+      header.style.backgroundColor = '#111';
     } else {
       header.style.backgroundColor = '#000';
     }
