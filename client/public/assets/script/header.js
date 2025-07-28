7bd7fa95de1d2030d87f5e0b604570dbbbf9a09a
spa29424 <alex@ericmentor.com>
2025-07-28 10:17:57 +0300

    Force delete client/public/assets/script/header.js

--- client/public/assets/script/header.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-    const header = document.getElementById('main-header');
-    if (window.scrollY > 30) {
-      header.style.backgroundColor = '#111';
-    } else {
-      header.style.backgroundColor = '#000';
-    }
-  });
-  
