694142301372d26000619ff4fa03086d57cf0312
spa29424 <alex@ericmentor.com>
2025-07-29 12:21:05 +0300

    Update client/src/pages/StaticHomeExact.tsx - CTA & UTM system

--- client/src/pages/StaticHomeExact.tsx
+++ client/src/pages/StaticHomeExact.tsx
@@ -674,7 +674,8 @@ function StaticHomeExact() {
 
       <section className="section-students">
         <h4 className="section-students__title testimonial-title">
-          This is what our <span className="highlight">students</span> have to say…
+          <span className="desktop-text">Real results, real feedback from our students</span>
+          <span className="mobile-text">Real results, real feedback.</span>
         </h4>
         <div className="video-wrapper">
           <iframe 
