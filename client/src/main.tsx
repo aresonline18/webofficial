6fb1279737385bae315611ff77bd6dd659a40a06
spa29424 <alex@ericmentor.com>
2025-11-02 18:04:57 +0100

    Initial sync: Add Lovable project to client/ folder (5 files)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=7cbad96a"; const _jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=7cbad96a"; const createRoot = __vite__cjsImport1_reactDom_client["createRoot"];
+import App from "/src/App.tsx?t=1762103076959";
+import "/src/index.css?t=1762103076959";
+createRoot(document.getElementById("root")).render(/*#__PURE__*/ _jsxDEV(App, {}, void 0, false, {
+    fileName: "/dev-server/src/main.tsx",
+    lineNumber: 5,
+    columnNumber: 53
+}, this));
 
-createRoot(document.getElementById("root")!).render(<App />);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4udHN4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IGNyZWF0ZVJvb3QgfSBmcm9tIFwicmVhY3QtZG9tL2NsaWVudFwiO1xuaW1wb3J0IEFwcCBmcm9tIFwiLi9BcHAudHN4XCI7XG5pbXBvcnQgXCIuL2luZGV4LmNzc1wiO1xuXG5jcmVhdGVSb290KGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKFwicm9vdFwiKSEpLnJlbmRlcig8QXBwIC8+KTtcbiJdLCJuYW1lcyI6WyJjcmVhdGVSb290IiwiQXBwIiwiZG9jdW1lbnQiLCJnZXRFbGVtZW50QnlJZCIsInJlbmRlciJdLCJtYXBwaW5ncyI6IjtBQUFBLFNBQVNBLFVBQVUsUUFBUSxtQkFBbUI7QUFDOUMsT0FBT0MsU0FBUyxZQUFZO0FBQzVCLE9BQU8sY0FBYztBQUVyQkQsV0FBV0UsU0FBU0MsY0FBYyxDQUFDLFNBQVVDLE1BQU0sZUFBQyxRQUFDSCJ9
